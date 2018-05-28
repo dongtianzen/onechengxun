88396b95de6a08bb707c98e9e9f422527e64e6d2
dongtianzen <dongtianzen@gmail.com>
2018-05-28 17:08:29 -0400

    dev bootstrap_material_design_controller

---
+++ web/themes/contrib/wan/js/bootstrap_material_design_controller.js
@@ -0,0 +1,9 @@
+
+jQuery(document).ready(function($) {
+  $(function () {
+    console.log(12345);
+    // jQuery.material.init();
+    $('body').bootstrapMaterialDesign();
+    console.log(339999);
+  });
+});
---
+++ web/themes/contrib/wan/js/test_controller.js
@@ -0,0 +1,7 @@
+
+jQuery(document).ready(function($) {
+  console.log('test_controller.js 34567');
+  // $(function () {
+  //   $('body').bootstrapMaterialDesign();
+  // });
+});
